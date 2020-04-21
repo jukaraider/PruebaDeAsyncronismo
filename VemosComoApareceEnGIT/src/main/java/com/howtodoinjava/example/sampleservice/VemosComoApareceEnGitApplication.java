4019f3f3730cf13d3e430846322485907edfa905
jukaraider <a788782@10.85.162.176>
2020-04-21 12:23:53 -0300

    .

---
+++ VemosComoApareceEnGIT/src/main/java/com/howtodoinjava/example/sampleservice/VemosComoApareceEnGitApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.example.sampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemosComoApareceEnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemosComoApareceEnGitApplication.class, args);
+	}
+
+}
